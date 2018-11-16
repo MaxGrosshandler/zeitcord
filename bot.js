a8dc4a272fac3c5fdb594a872ec5007943ab4c74
Max Grosshandler <maxgrosshandler@gmail.com>
2018-11-16 08:55:59 -0600

    more zeit testing

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ bot.on("ready", () => { // When the bot is ready
     console.log("Ready!"); // Log "Ready!"
 });
 
-bot.on("messageCreate", (msg) => { // When a message is created
+bot.on("messageCreate", async (msg) => { // When a message is created
     if(msg.content === "zping") { // If the message content is "!ping"
         bot.createMessage(msg.channel.id, "Pong!");
         // Send a message in the same channel with "Pong!"
